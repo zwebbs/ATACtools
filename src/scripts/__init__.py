f095ae29b1358f017ada8a10692f0e5fbb861197
zwebbs <zwebbs2021@gmail.com>
2022-11-02 16:25:37 -0500

    began work on annotateTSS script

---
+++ src/scripts/__init__.py
@@ -0,0 +1,4 @@
+# File name __init__.py
+# Created By: ZW
+# Created On: 2022-11-02
+# Purpose: allows running package scripts located in the src/ directory.
---
+++ src/scripts/annotateTSS.py
@@ -0,0 +1,8 @@
+# File Name: annotateTSS.py
+# Created By: ZW
+# Created On: 2022-11-02
+# Purpose: script to run TSS annotation of peaks in .bed or .narrowPeak format
+#  the script requires a bedfile to annotate as well as a gtf style annotation
+#  file for the genome of interest complete with the strand information, TxStart
+#  and TxEnd which conform to UCSC definitions. We recommend the RefSeq Curated.
+
